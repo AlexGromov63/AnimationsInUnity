e675dce304a2f87f3dc4adf029144fd595228028
Your Name <you@example.com>
2023-01-24 20:10:04 +0300

    first

---
+++ Assets/scripts/Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            GetComponent<Animator>().SetBool("isMashet", 
+                !GetComponent<Animator>().GetBool("isMashet"));
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Animator>().SetBool("isWalk",
+                !GetComponent<Animator>().GetBool("isWalk"));
+        }
+
+    }
+}
---
+++ Assets/scripts/SimpleAnim.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleAnim : MonoBehaviour
+{
+    public float StartDeltaX;
+    Vector3 startPos;
+    Vector3 startScale;
+    public float Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        startScale = transform.localScale;
+        transform.Translate(StartDeltaX, 0, 0);
+        StartCoroutine(Moving());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position != startPos)
+        {
+            transform.Translate(Speed, 0, 0);
+        }
+    }
+    IEnumerator Moving()
+    {
+        Vector3 pos = transform.position;
+        float t = 0;
+        while(t <= 1)
+        {
+            yield return null;
+            transform.position = Vector3.Lerp(pos, startPos, t);
+            t += 0.01f;
+        }
+    }
+    IEnumerator Scaling()
+    {
+        float t = 0;
+        while (t <= 1)
+        {
+            yield return new WaitForSeconds(0.01f);
+            transform.localScale = Vector3.Lerp(Vector3.zero, startScale, t);
+            t += 0.01f;
+        }
+    }
+}
